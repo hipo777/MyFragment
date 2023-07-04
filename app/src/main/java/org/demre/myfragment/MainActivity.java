8476419e989d25c49e2fbfec60dbf350827dc15e
hipolito <huaylla7@hotmail.com>
2023-07-03 22:27:36 -0400

    FirstFragment funcionando

--- app/src/main/java/org/demre/myfragment/MainActivity.java
+++ app/src/main/java/org/demre/myfragment/MainActivity.java
@@ -1,6 +1,8 @@
 package org.demre.myfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -8,11 +10,14 @@ import org.demre.myfragment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirstFragment fragment1;
+    private SecondFragment fragment2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
     }
 }
