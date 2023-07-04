1ef755f98095768903beb6d6a07b7a1aef66fee5
hipolito <huaylla7@hotmail.com>
2023-07-03 23:48:53 -0400

    SecondFragment funcionando

--- app/src/main/java/org/demre/myfragment/SecondFragment.java
+++ app/src/main/java/org/demre/myfragment/SecondFragment.java
@@ -12,35 +12,15 @@ import android.webkit.WebView;
 import org.demre.myfragment.databinding.FragmentFirstBinding;
 import org.demre.myfragment.databinding.FragmentSecondBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SecondFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     public SecondFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SecondFragment newInstance(String param1, String param2) {
         SecondFragment fragment = new SecondFragment();
         Bundle args = new Bundle();
@@ -53,9 +33,6 @@ public class SecondFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String websiteUrl = getArguments().getString("website_url");
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -67,7 +44,7 @@ public class SecondFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentSecondBinding binding = FragmentSecondBinding.inflate(getLayoutInflater(), container, false);
-
+        binding.webView.loadUrl(mParam1);
         return binding.getRoot();
     }
 }
